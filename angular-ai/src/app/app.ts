7373553aa24e328d1f372e15f8ae7add7b5b786d
Edymar Freitas Goulart <edymar-fg10@hotmail.com>
2026-06-27 13:52:30 -0300

    Angular AI

---
+++ angular-ai/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterLink],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected readonly title = signal('AI-Spring-Angular');
+}
